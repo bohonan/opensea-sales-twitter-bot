c3815678d8b0bf11ed207563cfa8e8d367c64510
Tom <tbohonan@militarycars.com>
2021-08-05 17:49:59 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# OpenSea Sales Twitter Bot
+
+A (very quickly put together) bot that monitors Opensea sales for a given collection & then posts them to Twitter.
+
+## Donations
+
+If you find this script/repo useful for your project, any ETH/token donations are greatly appreciated 🙏 
+
+Ethereum Address: 0xDCA88f66CEc8972D23DE7d5e69c40E087C92132f
+
+## Requirements
+
+- [Twitter Developer Account](https://developer.twitter.com/en/apply-for-access)
+
+- Heroku Account (Free Tier is fine)
+
+## Setup
+
+Once you have been granted access to a Twitter Developer Account, created a project there + created an account on Heroku
+
+- Clone/Fork/Copy this project to your local public/private git repo
+
+- Create a new Heroku app, link up your Github repo to it so you're able to push commits up and see builds happening in Heroku.
+
+- Make sure you are using `worker` dynos and not `web` dynos - can set this in the CLI your project with:
+
+```sh
+heroku ps:scale web=0
+heroku ps:scale worker=1
+```
+
+In the Settings section of your Heroku app you'll see a Config Vars section. Add the following config vars:
+
+- **CONSUMER_KEY** - Your Twitter App's Consumer Key
+- **CONSUMER_SECRET** - Your Twitter App's Consumer Secret
+- **ACCESS_TOKEN_KEY** - The Access Token Key of the Twitter Account your bot is posting from
+- **ACCESS_TOKEN_SECRET** - The Access Token Secret of the Twitter Account your bot is posting from
+- **OPENSEA_COLLECTION_SLUG** - The OpenSea collection name you wish to track (e.g. `cryptopunks`)
+
+## Tweet Content
+
+By default, in the tweet itself I am including an image of the item that was sold, the name of the item, the price in both ETH & USD, an external link if one exists + a couple of hashtags. Check out the [OpenSea Events API](https://docs.opensea.io/reference#retrieving-asset-events) and include whatever extra properties you'd like (such as seller/buyer addresses etc.).
+
+## Useful Resources
+
+If you are having trouble setting up your Twitter Developer project, Heroku project etc. the following resources may be of use
+
+- [Heroku - Deploying with Git](https://devcenter.heroku.com/articles/git)
+- [Twurl - Generate Access Token Key/Secret Locally](https://github.com/twitter/twurl)
+- [OpenSea Events API](https://docs.opensea.io/reference#retrieving-asset-events)
+
+## License
+
+This code is licensed under the [ISC License](https://choosealicense.com/licenses/isc/).
+
+Please include proper attribution if you fork or modify this project in some way. Thank you!
---
+++ app.js
@@ -0,0 +1,91 @@
+const axios = require('axios');
+const _ = require('lodash');
+const moment = require('moment');
+const { ethers } = require('ethers');
+const tweet = require('./tweet');
+
+function formatAndSendTweet(twitterData) {
+    const formattedTokenPrice = ethers.utils.formatEther(twitterData.totalPrice.toString());
+    const formattedUsdPrice = (formattedTokenPrice * twitterData.usdValue).toFixed(2);
+    const formattedPriceSymbol = (
+        (twitterData.tokenSymbol === 'WETH' || twitterData.tokenSymbol === 'ETH') 
+            ? 'Ξ' 
+            : ` ${twitterData.tokenSymbol}`
+    );
+
+    const tweetText = `${twitterData.tokenName} was purchased for ${formattedTokenPrice}${formattedPriceSymbol} ($${formattedUsdPrice}) #treeverse #ethereum ${twitterData.openseaLink}`;
+
+    console.log(tweetText);
+
+    return tweet.handleDupesAndTweet(twitterData.tokenName, tweetText, twitterData.image);
+}
+
+async function getLastestSaleData(collection) {
+    let res = await axios.post('https://api.opensea.io/graphql/', {
+        "id": "EventHistoryQuery",
+        "query": "query EventHistoryQuery(  $archetype: ArchetypeInputType  $bundle: BundleSlug  $collections: [CollectionSlug!]  $categories: [CollectionSlug!]  $chains: [ChainScalar!]  $eventTypes: [EventType!]  $cursor: String  $count: Int = 10  $showAll: Boolean = false  $identity: IdentityInputType) {  ...EventHistory_data_L1XK6}fragment AccountLink_data on AccountType {  address  user {    publicUsername    id  }  ...ProfileImage_data  ...wallet_accountKey  ...accounts_url}fragment AssetCell_asset on AssetType {  collection {    name    id  }  name  ...asset_url}fragment AssetQuantity_data on AssetQuantityType {  asset {    ...Price_data    id  }  quantity}fragment EventHistory_data_L1XK6 on Query {  assetEvents(after: $cursor, bundle: $bundle, archetype: $archetype, first: $count, categories: $categories, collections: $collections, chains: $chains, eventTypes: $eventTypes, identity: $identity, includeHidden: true) {    edges {      node {        assetQuantity {          asset @include(if: $showAll) {            ...AssetCell_asset            id          }          id        }        eventTimestamp        eventType        offerEnteredClosedAt        customEventName        price {          quantity          ...AssetQuantity_data          id        }        endingPrice {          quantity          ...AssetQuantity_data          id        }        seller {          ...AccountLink_data          id        }        winnerAccount {          ...AccountLink_data          id        }        id        __typename      }      cursor    }    pageInfo {      endCursor      hasNextPage    }  }}fragment Price_data on AssetType {  decimals  imageUrl  symbol  usdSpotPrice  assetContract {    blockExplorerLink    account {      chain {        identifier        id      }      id    }    id  }}fragment ProfileImage_data on AccountType {  imageUrl  address  chain {    identifier    id  }}fragment accounts_url on AccountType {  address  chain {    identifier    id  }  user {    publicUsername    id  }}fragment asset_url on AssetType {  assetContract { address  id  }  tokenId imageUrl traits(first: 100) {        edges {          node {            relayId            displayType            floatValue            intValue            traitType            value            id          }        }      }}fragment wallet_accountKey on AccountType {  address  chain {    identifier    id  }}",
+        "variables": {
+            "eventTypes": ["AUCTION_SUCCESSFUL"],
+            "cursor": null,
+            "count": 100,
+            "showAll": true,
+            "collections": [
+                collection
+            ],
+            "includeHiddenCollections": false,
+            "showContextMenu": false,
+            "shouldShowQuantity": false,
+            "sortAscending": false,
+            "sortBy": "LAST_SALE_DATE"
+        }
+    });
+    return res.data
+}
+
+function buildDataForTwitter(sale) {
+    return {
+        tokenName: sale.node.assetQuantity.asset.name,
+        image: sale.node.assetQuantity.asset.imageUrl,
+        openseaLink: `https://opensea.io/assets/${sale.node.assetQuantity.asset.assetContract.address}/${sale.node.assetQuantity.asset.tokenId}`,
+        totalPrice: sale.node.price.quantity,
+        usdValue: sale.node.price.asset.usdSpotPrice,
+        tokenSymbol: sale.node.price.asset.symbol,
+        sellersAddress: sale.node.seller.address,
+        buyersAddress: sale.node.winnerAccount.address
+    }
+}
+
+function isNewSaleInLastMinute(lastMinute, sale) {
+    let salesTimestamp = moment(sale.node.eventTimestamp, 'YYYY-MM-DDThh:mm:ss')
+    let diff = lastMinute.diff(salesTimestamp, 'seconds');
+    return (diff < 0);
+}
+
+function processAllSales(lastMinute, latestSalesData) {
+    sentTwitterData = []
+    const sales = latestSalesData.data.assetEvents.edges;
+    for(let index in sales) {
+        let sale = sales[index];
+        if(isNewSaleInLastMinute(lastMinute, sale)) {
+            if (sale.node.assetQuantity) {
+                twitterData = buildDataForTwitter(sale);
+                formatAndSendTweet(twitterData);
+                sentTwitterData.push(twitterData);
+            }
+        } else {
+            break
+        }
+    }
+    return sentTwitterData;
+}
+
+// Poll OpenSea every minute & retrieve all sales for a given collection in the last minute
+// Then pass those events over to the formatter before tweeting
+setInterval(async () => {
+    const lastMinute = moment().startOf('minute').subtract(59, "seconds")
+    try {
+        processAllSales(lastMinute, await getLastestSaleData("treeverse"));
+    } catch(error) {
+        console.log(error);
+    }
+}, 60000);
