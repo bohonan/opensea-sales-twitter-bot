dad4ba316725f0f0b8ac741207b46557222f9abe
Tom <tbohonan@militarycars.com>
2021-08-06 16:14:15 -0400

    add header data to make it look like a browser making the call

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ async function getLastestSaleData(collection) {
             "sortBy": "LAST_SALE_DATE"
         }
     }
-    let res = await axios.post('https://api.opensea.io/graphql/', postData);
+    let res = await axios.post('https://api.opensea.io/graphql/', postData, {
+        headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+    });
     return res.data
 }
 
